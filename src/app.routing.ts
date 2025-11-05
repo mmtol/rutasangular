da73a32787f741b699c903ec5bed83d2ac9a0650
mmtol <mmigueltolosana@gmail.com>
2025-11-05 13:28:20 +0100

    aaaa

---
+++ src/app.routing.ts
@@ -0,0 +1,31 @@
+import { HomeComponent } from "./components/home.component/home.component";
+import { CineComponent } from "./components/cine.component/cine.component";
+import { MusicaComponent } from "./components/musica.component/musica.component";
+
+//necesitamos modulos que estan dentro de angular
+import { Routes } from "@angular/router";
+import { RouterModule } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
+
+//necesitamos un array de tipo routes con las rutas
+const appRoutes:Routes=
+[
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"cine",
+        component:CineComponent
+    },
+    {
+        path:"musica",
+        component:MusicaComponent
+    }
+]
+
+//desde esta clase exportamos el array de routes como provider
+export const appRoutingProvider: any[] = [];
+//las propias rutas a exportar
+export const routing:ModuleWithProviders<any> =
+RouterModule.forRoot(appRoutes);
--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,17 +1,26 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from '../app.routing';
+import { appRoutingProvider } from '../app.routing';
 
 import { App } from './app';
+import { HomeComponent } from '../components/home.component/home.component';
+import { CineComponent } from '../components/cine.component/cine.component';
+import { MusicaComponent } from '../components/musica.component/musica.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    HomeComponent,
+    CineComponent,
+    MusicaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, routing
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    appRoutingProvider
   ],
   bootstrap: [App]
 })
