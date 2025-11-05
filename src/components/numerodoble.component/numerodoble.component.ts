c285a5df736432b2ea378f013f23bdbb6f7649b2
mmtol <mmigueltolosana@gmail.com>
2025-11-05 13:51:11 +0100

    aaaaaaa

---
+++ src/components/numerodoble.component/numerodoble.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numerodoble.component',
+  standalone: false,
+  templateUrl: './numerodoble.component.html',
+  styleUrl: './numerodoble.component.css',
+})
+export class NumerodobleComponent {
+
+}
