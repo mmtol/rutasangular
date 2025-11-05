5d1a6767d459cccd06e66a58aa4d62ede36dd933
mmtol <mmigueltolosana@gmail.com>
2025-11-05 13:46:10 +0100

    rutas

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,13 +7,15 @@ import { App } from './app';
 import { HomeComponent } from '../components/home.component/home.component';
 import { CineComponent } from '../components/cine.component/cine.component';
 import { MusicaComponent } from '../components/musica.component/musica.component';
+import { MenuComponent } from '../components/menu.component/menu.component';
 
 @NgModule({
   declarations: [
     App,
     HomeComponent,
     CineComponent,
-    MusicaComponent
+    MusicaComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, routing
